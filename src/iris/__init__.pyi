2a43b18df889c5960274c3d40d75bdb84773e373
grongierisc <guillaume.rongier@intersystems.com>
2024-02-02 11:26:02 +0100

    Refactor code to improve performance and readability

--- src/iris/__init__.pyi
+++ src/iris/__init__.pyi
@@ -1,138 +1,187 @@
-from typing import Any, Optional
-from dataclasses import dataclass
-
-@dataclass
-class PreparedQuery:
-    def execute(self, **kwargs) -> Any:
-        """
-        Execute a prepared query, you can pass values
-        """
-        ...
-
-@dataclass
-class SQLMethods:
+from __future__ import annotations
+from typing import Any
+from iris.iris_ipm import ipm
+from . import iris_ipm
+__all__ = [ 'check_status', 'cls', 'execute', 'gref', 'ipm', 'iris_ipm', 'lock', 'os', 'ref', 'routine', 'sql', 'system', 'tcommit', 'tlevel', 'trollback', 'trollbackone', 'tstart', 'unlock', 'utils']
+def check_status(self, status):
+    """
+    Raises an exception on an error status, or returns None if no error condition occurs.
+    Example: iris.check_status(st) checks the status code st to see if it contains an error.
+    """
+def cls(self, class_name):
+    """
+    Returns a reference to an InterSystems IRIS class.
+    Example: iris.cls("%SYSTEM.INetInfo").LocalHostName() calls a method in the class %SYSTEM.INetInfo.
+    """
+def execute(self, statements):
+    """
+    execute IRIS statements.
+    Example: iris.execute("set x="Hello"\nw x,!\n") returns nothing.
+    """
+def gref(self, global_name):
+    """
+    Returns a reference to an InterSystems IRIS global.
+    Example: g = iris.gref("^foo") sets g to a reference to global ^foo
+    """
+def lock(self, lock_list, timeout_value, locktype):
+    """
+    Sets locks, given a list of lock names, an optional timeout value (in seconds), and an optional locktype.
+    Example: iris.lock(["^foo","^bar"], 30, "S") sets locks "^foo" and "^bar", waiting up to 30 seconds, and using shared locks.
+    """
+def ref(self, value):
+    """
+    Creates an iris.ref object with a specified value.
+    Example: iris.ref("hello") creates an iris.ref object with the value "hello"
+    """
+def routine(self, routine, args):
+    """
+    Invokes an InterSystems IRIS routine, optionally at a given tag.
+    Example: iris.routine("Stop^SystemPerformance", "20211221_160620_test") calls tag Stop in routine ^SystemPerformance.
+    """
+def tcommit(self):
+    """
+    Marks a successful end of an InterSystems IRIS transaction.
+    Example: iris.commit() marks the successful end of a transaction and decrements the nesting level by 1
+    """
+def tlevel(self):
+    """
+    Detects whether a transaction is currently in progress and returns the nesting level. Zero means not in a transaction.
+    Example: iris.tlevel() returns the current transaction nesting level, or zero if not in a transaction
+    """
+def trollback(self):
+    """
+    Terminates the current transaction and restores all journaled database values to their values at the start of the transaction.
+    Example: iris.trollback() rolls back all current transactions in progress and resets the transaction nesting level to 0
+    """
+def trollbackone(self):
+    """
+    Rolls back the current level of nested transactions, that is, the one initiated by the most recent tstart().
+    Example: iris.trollbackone() rolls back the current level of nested transactions and decrements the nesting level by 1
+    """
+def tstart(self):
+    """
+    Starts an InterSystems IRIS transaction.
+    Example: iris.tstart() marks the beginning of a transaction.
+    """
+def unlock(self, lock_list, timout_value, locktype):
+    """
+    Removes locks, given a list of lock names, an optional timeout value (in seconds), and an optional locktype.
+    Example: iris.unlock(["^foo","^bar"], 30, "S") removes locks "^foo" and "^bar", waiting up to 30 seconds, and using shared locks.
+    """
+def utils(self):
+    """
+    Returns a reference to the InterSystems IRIS utilities class.
+    Example: iris.utils().$Job() returns the current job number.
+    """
+# stubs for the sql object
+class sql:
+    """
+    The sql object provides access to the InterSystems IRIS SQL API.
+    """
     def exec(self, query: str) -> Any:
         """
         Execute a query
         """
-        ...
-    
     def prepare(self, query: str) -> PreparedQuery:
         """
         Prepare a query
         """
-        ...
+    class PreparedQuery:
+        def execute(self, **kwargs) -> Any:
+            """
+            Execute a prepared query, you can pass values
+            """
+# stubs for the system object
+class system:
+    """
+    The system object provides access to the InterSystems IRIS system API.
+    The following classes are available:
+    'DocDB', 'Encryption', 'Error', 'Event', 'Monitor', 'Process', 'Python', 'SQL', 'SYS', 'Security', 'Semaphore', 'Status', 'Util', 'Version'
+    """
+    class DocDB:
+        """
+        The DocDB class provides access to the InterSystems IRIS Document Database API.
+        The following methods are available:
+        'CreateDatabase', 'DropAllDatabases', 'DropDatabase', 'Exists', 'GetAllDatabases', 'GetDatabase', 'Help'
+        """
+        def CreateDatabase(self, name: str, path: str, **kwargs) -> Any:
+            """
+            Create a database
+            """
+        def DropAllDatabases(self) -> Any:
+            """
+            Drop all databases
+            """
+        def DropDatabase(self, name: str) -> Any:
+            """
+            Drop a database
+            """
+        def Exists(self, name: str) -> Any:
+            """
+            Check if a database exists
+            """
+        def GetAllDatabases(self) -> Any:
+            """
+            Get all databases
+            """
+        def GetDatabase(self, name: str) -> Any:
+            """
+            Get a database
+            """
+        def Help(self) -> Any:
+            """
+            Get help
+            """
+    def Encryption(self) -> Any:
+        """
+        The Encryption class provides access to the InterSystems IRIS Encryption API.
+        """
+    def Error(self) -> Any:
+        """
+        The Error class provides access to the InterSystems IRIS Error API.
+        """
+    def Event(self) -> Any:
+        """
+        The Event class provides access to the InterSystems IRIS Event API.
+        """
+    def Monitor(self) -> Any:
+        """
+        The Monitor class provides access to the InterSystems IRIS Monitor API.
+        """
+    def Process(self) -> Any:
+        """
+        The Process class provides access to the InterSystems IRIS Process API.
+        """
+    def Python(self) -> Any:
+        """
+        The Python class provides access to the InterSystems IRIS Python API.
+        """
+    def SQL(self) -> Any:
+        """
+        The SQL class provides access to the InterSystems IRIS SQL API.
+        """
+    def SYS(self) -> Any:
+        """
+        The SYS class provides access to the InterSystems IRIS SYS API.
+        """
+    def Security(self) -> Any:
+        """
+        The Security class provides access to the InterSystems IRIS Security API.
+        """
+    def Semaphore(self) -> Any:
+        """
+        The Semaphore class provides access to the InterSystems IRIS Semaphore API.
+        """
+    def Status(self) -> Any:
+        """
+        The Status class provides access to the InterSystems IRIS Status API.
+        """
+    def Util(self) -> Any:
+        """
+        The Util class provides access to the InterSystems IRIS Util API.
+        """
+    def Version(self) -> Any:
+        """
+        The Version class provides access to the InterSystems IRIS Version API.
+        """
 
-def check_status(status: Any) -> None:
-    """
-    Raises an exception if status contains an error.
-    Returns None if no error condition occurs.
-    """
-    ...
-
-def cls(class_name: str) -> Any:
-    """
-    Returns a reference to an InterSystems IRIS class. This allows you access the
-    properties and methods of that class in the same way you would a with a Python
-    class. You can use iris.cls() to access both built-in InterSystems IRIS classes
-    or custom InterSystems IRIS classes you write yourself.
-    """
-    ...
-
-def execute(statement: str) -> Optional[Any]:
-    """
-    Executes an ObjectScript statement and optionally returns a value.
-    """
-    ...
-
-def gref(global_name: str) -> Any:
-    """
-    Returns a reference to an InterSystems IRIS global. The global may or may
-    not already exist.
-    """
-    ...
-
-def lock(lock_list: list[str], timeout_value: int = None, locktype: str = None) -> bool:
-    """
-    Sets locks, given a list of lock names, an optional timeout value (in seconds),
-    and an optional lock type. If locktype is "S", this indicates a shared lock.\n
-    In InterSystems IRIS, a lock is used to prevent more than one user or process
-    from accessing or modifying the same resource (usually a global) at the same
-    time. For example, a process that writes to a resource should request an exclusive
-    lock (the default) so that another process does not attempt to read or write to that
-    resource simultaneously. A process that reads a resource can request a shared lock so
-    that other processes can read that resource at the same time, but not write to that
-    resource. A process can specify a timeout value, so that it does not wait forever
-    waiting for a resource to become available.
-    """
-    ...
-
-def ref(value: Any) -> Any:
-    """
-    Creates an iris.ref object with a specified value. This is useful for
-    situations when you need to pass an argument to an ObjectScript method by reference.
-    """
-    ...
-
-def routine(routine_name: str, **kwargs) -> Any:
-    """
-    Invokes an InterSystems IRIS routine, optionally at a given tag. Any arguments
-    that need to be passed in the call are comma-delimited, following the name of
-    the routine.
-    """
-    ...
-
-def tcommit():
-    """
-    Marks the successful end of an InterSystems IRIS transaction.
-    """
-    ...
-
-def tlevel():
-    """
-    Detects whether a transaction is currently in progress and returns the
-    nesting level. A call to iris.tstart() increments the nesting level, and a
-    call to iris.tcommit() decrements the nesting level. A value of zero means not
-    in a transaction.
-    """
-    ...
-
-def trollback():
-    """
-    Rolls back all current transactions in progress and restores all journaled
-    database values to their values at the start of the initial transaction. It
-    also resets the transaction nesting level to 0.
-    """
-    ...
-
-def trollbackone():
-    """
-    Rolls back the current level of nested transactions, that is, the one initiated
-    by the most recent iris.tstart(). It also decrements the transaction nesting
-    level by 1.
-    """
-    ...
-
-def tstart():
-    """
-    Marks the start of an InterSystems IRIS transaction.\n
-    A transaction is a group of commands that must all complete in order for
-    the transaction to be considered successful. For example, if you have a
-    transaction that transfers a sum of money from one bank account to another,
-    the transaction is only successful if withdrawing the money from the first
-    account and depositing it into the second account are both successful. If
-    the transaction fails, the database can be rolled back to the state it was
-    in before the start of the transaction.
-    """
-    ...
-
-def unlock(lock_list: list[str], timeout_value: int = None, locktype: str = None) -> bool:
-    """
-    Removes locks, given a list of lock names, an optional timeout value
-    (in seconds), and an optional lock type.\n
-    If your code sets locks to control access to resources, it should unlock
-    them when it is done using those resources.
-    """
-    ...
-
-sql: SQLMethods = ...
